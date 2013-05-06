e57782cc932325841f41b7c8421f19ea7819378e
growthhuman <growthhuman@gmail.com>
2013-05-06 14:34:52 +0900

    batファイルが競合してたの修正

--- WebContent/WEB-INF/db/run.bat
+++ WebContent/WEB-INF/db/run.bat
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 java -cp C:\eclipse\hsqldb\lib\hsqldb.jar org.hsqldb.server.Server -database mydb
-=======
-java -cp ..\lib\hsqldb.jar org.hsqldb.server.Server -database mydb
->>>>>>> 0687cd5cc84ca8bae3ae0280f420794b0044b8ae
--- WebContent/WEB-INF/db/runmanager.bat
+++ WebContent/WEB-INF/db/runmanager.bat
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 java -cp C:\eclipse\hsqldb\lib\hsqldb.jar org.hsqldb.util.DatabaseManagerSwing -url jdbc:hsqldb:hsql://localhost
-=======
-java -cp ..\lib\hsqldb.jar org.hsqldb.util.DatabaseManagerSwing -url
->>>>>>> 0687cd5cc84ca8bae3ae0280f420794b0044b8ae
